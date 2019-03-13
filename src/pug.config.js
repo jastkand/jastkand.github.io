9074f9ce580050dada041fd1e6847901b5deedd8
Andrey Krivko <jastkand@gmail.com>
2019-03-13 21:27:46 +0600

    Migrate from jekyll to nothing

---
+++ src/pug.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  locals: {
+    siteTitle: "krivko.me",
+    author: "Andrey Krivko",
+    contacts: [
+      { name: "github", href: "https://github.com/jastkand" },
+      { name: "twitter", href: "https://twitter.com/jastkand" },
+      { name: "devto", href: "https://dev.to/jastkand" },
+    ]
+  }
+};
