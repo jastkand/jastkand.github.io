e81066903c82c2727859d441a2f513a0c4abde83
Andrey Krivko <jastkand@gmail.com>
2021-05-30 22:53:47 +0600

    tweaks and fixes

--- src/pug.config.js
+++ src/pug.config.js
@@ -1,11 +1,12 @@
 module.exports = {
   locals: {
-    siteTitle: "krivko.me",
-    author: "Andrey Krivko",
+    siteTitle: 'krivko.me',
+    siteDescription: 'Andrey Krivko\'s personal page',
+    author: 'Andrey Krivko',
     contacts: [
-      { name: "github", href: "https://github.com/jastkand" },
-      { name: "twitter", href: "https://twitter.com/jastkand" },
-      { name: "devto", href: "https://dev.to/jastkand" },
+      { name: 'github', href: 'https://github.com/jastkand', title: 'Github' },
+      { name: 'twitter', href: 'https://twitter.com/jastkand', title: 'Twitter' },
+      { name: 'devto', href: 'https://dev.to/jastkand', title: 'Blog' },
     ]
   }
 };
